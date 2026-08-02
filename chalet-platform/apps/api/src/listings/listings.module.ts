25a105d2f1e198ffa7154a2ce4966527174573b8
SeifMoustafa714 <seifmoustafa714@gmail.com>
2026-08-02 22:05:29 +0300

    Add ReviewsController to ListingsModule

--- chalet-platform/apps/api/src/listings/listings.module.ts
+++ chalet-platform/apps/api/src/listings/listings.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ListingsController } from './listings.controller';
+import { ReviewsController } from './reviews.controller';
 import { ListingsService } from './listings.service';
 
 @Module({
-  controllers: [ListingsController],
+  controllers: [ListingsController, ReviewsController],
   providers: [ListingsService],
 })
 export class ListingsModule {}
