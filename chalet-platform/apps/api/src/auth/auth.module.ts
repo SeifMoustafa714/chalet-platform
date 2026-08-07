71e46aafa02d57d5be7992cd8dbf0505aeb8043e
SeifMoustafa714 <seifmoustafa714@gmail.com>
2026-08-07 16:01:41 +0300

    Add MailService to AuthModule providers

--- chalet-platform/apps/api/src/auth/auth.module.ts
+++ chalet-platform/apps/api/src/auth/auth.module.ts
@@ -4,10 +4,11 @@ import { PassportModule } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtStrategy } from './strategies/jwt.strategy';
+import { MailService } from './mail.service';
 
 @Module({
   imports: [PassportModule, JwtModule.register({})],
-  providers: [AuthService, JwtStrategy],
+  providers: [AuthService, JwtStrategy, MailService],
   controllers: [AuthController],
   exports: [AuthService],
 })
